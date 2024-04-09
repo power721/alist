0ade1eaec21b039c65549d25426724bd8bc36991
Harold <power0721@gmail.com>
2024-04-09 19:40:43 +0800

    quark share

--- internal/bootstrap/storage.go
+++ internal/bootstrap/storage.go
@@ -30,6 +30,7 @@ func LoadStorages() {
 					strings.Contains(msg, "share_link cannot be found") ||
 					strings.Contains(msg, "share_pwd is not valid") ||
 					strings.Contains(msg, "取消") ||
+					strings.Contains(msg, "已失效") ||
 					strings.Contains(msg, "invalid") ||
 					strings.Contains(msg, "no route to host") {
 					log.Warnf("[%d] failed get enabled storages [%s], %+v",
