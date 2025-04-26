3dcb875f314bf906789b5c69823e92b63280bd08
Har01d <power0721@gmail.com>
2025-04-26 10:49:38 +0800

    update alist to v3.44.0

--- drivers/123/meta.go
+++ drivers/123/meta.go
@@ -6,19 +6,12 @@ import (
 )
 
 type Addition struct {
-	Username       string `json:"username"`
-	Password       string `json:"password"`
-	UseQrCodeLogin bool   `json:"use_qr_code_login"`
-	UniID          string `json:"uni_id"`
+	Username string `json:"username" required:"true"`
+	Password string `json:"password" required:"true"`
 	driver.RootID
 	//OrderBy        string `json:"order_by" type:"select" options:"file_id,file_name,size,update_at" default:"file_name"`
 	//OrderDirection string `json:"order_direction" type:"select" options:"asc,desc" default:"asc"`
-	AccessToken  string `json:"accesstoken" type:"text"`
-	PlatformType string `json:"platformType" type:"select" options:"android,tv" default:"android" required:"true"`
-	DeviceName   string `json:"devicename" default:"Xiaomi"`
-	DeiveType    string `json:"devicetype" default:"M1810E5A"`
-	OsVersion    string `json:"osversion" default:"Android_8.1.0"`
-	LoginUuid    string `json:"loginuuid" default:""`
+	AccessToken string
 }
 
 var config = driver.Config{
--- drivers/lark.go
+++ drivers/lark.go
@@ -1,4 +1,3 @@
-//go:build (linux || darwin || windows) && (amd64 || arm64)
 // +build linux darwin windows
 // +build amd64 arm64
 
