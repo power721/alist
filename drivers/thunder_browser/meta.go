06bdc0867ac5a57c6b3c0e19ba830165bd6628e3
Harold Li <Harold.Li@dell.com>
2025-06-06 15:46:59 +0800

    set Concurrency

--- drivers/thunder_browser/meta.go
+++ drivers/thunder_browser/meta.go
@@ -48,6 +48,8 @@ type ExpertAddition struct {
 	UseVideoUrl bool `json:"use_video_url"`
 	// 移除方式
 	RemoveWay string `json:"remove_way" required:"true" type:"select" options:"trash,delete"`
+
+	Concurrency int `json:"concurrency" type:"number" default:"2"`
 }
 
 // GetIdentity 登录特征,用于判断是否重新登录
